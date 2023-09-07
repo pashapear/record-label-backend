53f0b5a0dff8c8c4a38b5900f6c33c13ac80d5f5
Pasha Petrosyan <34192619+pashapear@users.noreply.github.com>
2023-09-07 13:52:02 -0500

    syntax update

--- schemas/artist.ts
+++ schemas/artist.ts
@@ -1,40 +1,48 @@
-// import {defineType, defineField, defineArrayMember} from 'sanity'
+import {defineType, defineField, defineArrayMember} from 'sanity'
 
-export default {
+export default defineType({
   name: 'artist',
   type: 'document',
   title: 'Artist',
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
       title: 'Name',
-    },
-    {
+    }),
+    defineField({
+      name: 'active',
+      type: 'boolean',
+      title: 'Active',
+    }),
+    defineField({
       name: 'slug',
       type: 'slug',
       title: 'Slug',
-    },
-    {
+    }),
+    defineField({
       name: 'image',
       type: 'image',
       title: 'Image',
-    },
-    {
+    }),
+    defineField({
       name: 'bio',
       type: 'string',
       title: 'Bio',
-    },
-    {
+    }),
+    defineField({
       name: 'location',
       type: 'string',
       title: 'Location',
-    },
-    {
+    }),
+    defineField({
       name: 'links',
       type: 'array',
       title: 'Links',
-      of: [{type: 'link'}],
-    },
+      of: [defineArrayMember({type: 'link'})],
+    }),
   ],
-}
+  initialValue: {
+    active: true,
+  },
+})
--- schemas/release.ts
+++ schemas/release.ts
@@ -1,44 +1,46 @@
-export default {
+import {defineType, defineField, defineArrayMember} from 'sanity'
+
+export default defineType({
   name: 'release',
   type: 'document',
   title: 'Release',
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
       title: 'Name',
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       type: 'slug',
       title: 'Slug',
-    },
-    {
-      name: 'Image',
+    }),
+    defineField({
+      name: 'image',
       type: 'image',
       title: 'Image',
-    },
-    {
+    }),
+    defineField({
       name: 'catalogNumber',
       type: 'number',
       title: 'Catalog Number',
-    },
-    {
+    }),
+    defineField({
       name: 'artist',
       type: 'reference',
       title: 'Artist',
       to: [{type: 'artist'}],
-    },
-    {
+    }),
+    defineField({
       name: 'releaseDate',
       type: 'date',
       title: 'Release Date',
-    },
-    {
-      name: 'Links',
+    }),
+    defineField({
+      name: 'links',
       type: 'array',
       title: 'Links',
-      of: [{type: 'link'}],
-    },
+      of: [defineArrayMember({type: 'link'})],
+    }),
   ],
-}
+})
